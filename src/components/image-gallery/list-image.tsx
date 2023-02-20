57c994a19877a1fad145b2a41e3a68764541ddc1
Marc Inglis <marcinglis91@hotmail.com>
2023-02-21 10:04:11 +1100

    Images displaying; information pulling from DB

--- src/components/image-gallery/list-image.tsx
+++ src/components/image-gallery/list-image.tsx
@@ -1,74 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ImagesProps } from '../interfaces/images';
+import myImage from '../../../backend/images/me.png';
 
-const GalleryImages = (props: ImagesProps) => (
-  <tr>
-    <td>{props.src}</td>
-    <td>{props.width}</td>
-    <td>{props.height}</td>
-    <td>{props.country}</td>
-    <td>{props.subregion}</td>
-    <td>{props.caption}</td>
-  </tr>
-)
-
-interface State {
-  galleries: ImagesProps[];
+interface ImageData {
+  _id: string;
+  src: string;
+  width: number;
+  height: number;
+  country: string;
+  subregion: string;
+  caption: string;
 }
 
-export default class ImagesList extends Component {
-  state: State = {
-    galleries: []
-  };
+const GalleryImages = ({ src, width, height, country, subregion, caption }: ImageData) => (
+  <tr>
+    <td>
+      <img src={`http://localhost:5000/${src}`} width={width} height={height} />
+    </td>
+    <td>{country}</td>
+    <td>{subregion}</td>
+    <td>{caption}</td>
+  </tr>
+);
 
-  componentDidMount() {
-    axios.get('http://localhost:5000/images/')
+const ImagesList = () => {
+  const [galleries, setGalleries] = useState<ImageData[]>([]);
+
+  useEffect(() => {
+    axios.get<ImageData[]>('http://localhost:5000/images/')
       .then(response => {
-        this.setState({ galleries: response.data })
+        setGalleries(response.data);
       })
       .catch((error) => {
         console.log(error);
-      })
-  }
+      });
+  }, []);
 
-  imageList() {
-    return this.state.galleries.map((currentImage: ImagesProps) => {
-      return <GalleryImages {...currentImage} key={currentImage._id} />;
-    });
-  }
+  
 
-  render() {
-    return (
-      <div>
-        <h3>Logged images</h3>
-        <table className="table table-auto	">
-          <thead className="thead-light">
-            <tr>
-              <th>Source</th>
-              <th>Country</th>
-              <th>Subregion</th>
-              <th>Caption</th>
-            </tr>
-          </thead>
-          <tbody>
-          { this.imageList().map(image => {
-            const currentImage = image.props;
-            return (
-              <tr key={currentImage._id}>
-                <td>
-                  <img src={currentImage.src} width={currentImage.width} height={currentImage.height} />
-                  
-                </td>
-                <td>{currentImage.country}</td>
-                <td>{currentImage.subregion}</td>
-                <td>{currentImage.caption}</td>
-              </tr>
-            )
-          })}
+  return (
+    <div>
+      <h3>Logged images</h3>
+      <table className="table table-auto">
+        <thead className="thead-light">
+          <tr>
+            <th>Source</th>
+            <th>Country</th>
+            <th>Subregion</th>
+            <th>Caption</th>
+          </tr>
+        </thead>
+        <tbody>
+          {galleries.map((image) => (
+            <GalleryImages {...image} key={image._id} />
+          ))}
         </tbody>
-        </table>
-      </div>
-    )
-  }
-}
+      </table>
+    </div>
+  );
+};
+
+export default ImagesList;
