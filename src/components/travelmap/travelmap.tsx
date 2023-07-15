fdc06b61c11562b17219f2446f6ca0b69007ef4e
Marc Inglis <marcinglis91@hotmail.com>
2023-07-15 16:08:32 +1000

    travelmap city display. add photo count function

--- src/components/travelmap/travelmap.tsx
+++ src/components/travelmap/travelmap.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ComposableMap, Geographies, Geography} from "react-simple-maps";
-
+import { ComposableMap, Geographies, Geography, Marker} from "react-simple-maps";
+import './styles.css'
 
 const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 type GeoData = {
@@ -14,11 +14,13 @@ type GeoData = {
   };
 
 const TravelMap: React.FC<TravelMapProps> = ({geographies, geo}) =>{
-
+const rng:number = Math.floor((Math.random()*100));
     return(
-        <div className='bg-gray-700 grid grid-cols-1 md:grid-cols-2'>
-            <h1>Travel Map Component</h1>
-            <ComposableMap
+        <div className='bg-gray-700 grid grid-cols-2 md:grid-cols-2'>
+          <div> <h1 className='text-xl text-left text-white'>Travel Map Component</h1>
+            <p className='text-left text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum enim exercitationem provident vero placeat asperiores aperiam voluptas praesentium, inventore, corrupti cum odio delectus cupiditate dolorem itaque eaque magnam. Quasi, officia.</p>
+</div><div>
+                       <ComposableMap
       width={800}
       height={800}
       projection="geoAzimuthalEqualArea"
@@ -39,7 +41,75 @@ const TravelMap: React.FC<TravelMapProps> = ({geographies, geo}) =>{
           ))
         }
       </Geographies>
+      
+      <Marker coordinates={[2.3522, 48.8566]} fill="#777">
+        <circle r={rng}/>
+        <text className="text" >
+          Paris, photos:{rng}
+        </text>
+        
+      </Marker>
+
+      <Marker coordinates={[-0.118092, 51.509865]} fill="#777">
+        <circle r='20' />
+        <text className="text" >
+          London
+        </text>
+        
+      </Marker>
+
+      <Marker coordinates={[-3.2, 55.9]} fill="#777">
+        <circle r='18' />
+        <a href="http://localhost:3000/image-gallery">
+        <text className="text" >
+          Edinburgh
+        </text></a>
+        
+      </Marker>
+
+      <Marker coordinates={[13.4050, 52.52]} fill="#777">
+        <circle r='23' />
+        <text className="text" >
+          Berlin
+        </text>
+        
+      </Marker>
+      
+      <Marker coordinates={[2.1686, 41.3874]} fill="#777">
+        <circle r='23'/>
+        <text className="text" >
+          Barcelona, photos:{rng}
+        </text>
+        
+      </Marker>
+
+      <Marker coordinates={[-8.6291, 41.1579]} fill="#777">
+        <circle r='20' />
+        <text className="text" >
+          Porto
+        </text>
+        
+      </Marker>
+
+      <Marker coordinates={[23.3219, 42.6977]} fill="#777">
+        <circle r='18' />
+        <a href="http://localhost:3000/image-gallery">
+        <text className="text" >
+          Sofia
+        </text></a>
+        
+      </Marker>
+
+      <Marker coordinates={[-21.9408, 64.1470]} fill="#777">
+        <circle r='23' />
+        <text className="text" >
+        Reykjavík
+        </text>
+        
+      </Marker>
+      
     </ComposableMap>
+        </div>
         </div>
     )   
 
--- src/react-simple-maps.d.ts
+++ src/react-simple-maps.d.ts
@@ -2,5 +2,6 @@ declare module 'react-simple-maps' {
     export const ComposableMap: React.ComponentType<any>;
     export const Geographies: React.ComponentType<any>;
     export const Geography: React.ComponentType<any>;
+    export const Marker: React.ComponentType<any>;
     // Add other exported components and types as needed
   }
