cb8afa68ad40adba729c9dc4ee7158e206eb2781
skateallday <marcinglis91@hotmail.com>
2023-11-09 11:17:15 +1000

    b

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend URL
+  origin: 'https://alisonabroad.onrender.com', // Replace with your frontend URL
   methods: 'GET, POST, PUT, DELETE',
   credentials: true, // Enable sending cookies across origins
 }));
