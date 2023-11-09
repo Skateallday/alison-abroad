57eca174855e193a9351b924a640b93f1a1ff29c
skateallday <marcinglis91@hotmail.com>
2023-11-09 10:31:10 +1000

    updating production URL

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 //config.ts
 const config = {
-    apiUrl:'http://localhost:5000', // Development URL
+    apiUrl:'https://alisonabroad.onrender.com', // Production URL
 };
 
 export default config;
