866250a958b04710121106c57effa6a5a6cea4b5
Marc <marcinglis91@hotmail.com>
2024-04-30 18:41:17 +1000

    Country and region buttons are now working correctly.

--- src/components/image-gallery/create-image.tsx
+++ src/components/image-gallery/create-image.tsx
@@ -6,9 +6,7 @@ import EditImage from "./edit-image";
 const CreateImage = () => {
   const [isForm, setIsForm] = useState("Single");
 
-  const handleSingleClick = () => {
-    setIsForm("Single");
-  };
+
 
   const handleMultiClick = () => {
     setIsForm("Multi");
@@ -21,8 +19,8 @@ const CreateImage = () => {
   return (
     <div>
     <div className="p-4">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleSingleClick}>Single Image</button>
       <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handleMultiClick}>Multiple Images</button>
+       
       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handleEditClick}>Edit Images</button>
 
     </div>
--- src/components/image-gallery/image-gallery.tsx
+++ src/components/image-gallery/image-gallery.tsx
@@ -4,16 +4,22 @@ import { getImages } from "../api/getImages";
 
 interface Image {
   country: string;
-  // Add other properties of the image
+  subregion: string;
 }
 
 const ImageGallery: React.FC = () => {
   const [country, setCountry] = useState<string>("Scotland");
   const [countries, setCountries] = useState<string[]>([]);
+  const [subregion, setsubregion ] = useState<string>("Glasgow");
+  const [subregions, setsubregions] = useState<string[]>([]);
+
+
 
   useEffect(() => {
       getImages()
       .then((response : {data: Image[]}) => {
+        console.log(response.data); // Log the data property
+
         const countriesArray = response.data
           .map((image) => image.country)
           .reduce<string[]>((uniqueCountries, country) => {
@@ -25,25 +31,61 @@ const ImageGallery: React.FC = () => {
           }, [])
           .map((country) => country.charAt(0).toUpperCase() + country.slice(1));
         setCountries(countriesArray);
-      })
+      
+      const subregionsArray = response.data
+      .filter((image) => image.country === country) // Filter images by selected country
+      .map((image) => image.subregion)
+      .reduce<string[]>((uniquesubregions, subregion) => {
+        // Only keep the first occurrence of the subregion
+        if (!uniquesubregions.includes(subregion)) {
+          uniquesubregions.push(subregion);
+        }
+        return uniquesubregions;
+      }, [])
+      .map((subregion) => subregion.charAt(0).toUpperCase() + subregion.slice(1));
+      setsubregions(subregionsArray);
+  })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [country]);
 
+  const handleCountryChange = (selectCountry: string) => {
+    setCountry(selectCountry);
+  };
+
+  const handleSubregionChange = (selectSubregion: string) => {
+    setCountry(selectSubregion);
+  };
 
   return (
     <div className="App-header">
       <div className="justify-center text-left p-10">
         <h1 className="text-3xl py-5">Here are my {country} Images</h1>
+        <h2 className="text-2xl py-5">Please select a country and subregion below</h2>
         <div className="flex flex-wrap p-4">
           {countries.map((countryOption) => (
             <button
               key={countryOption}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mr-4 mb-4"
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mr-4 mb-4 ${
+                country === countryOption ? "bg-blue-700" : ""
+              }`}
               onClick={() => setCountry(countryOption)}
             >
               {countryOption}
+            </button>
+          ))}
+        </div>
+        <div className="flex flex-wrap p-4">
+          {subregions.map((subregionOption) => (
+            <button
+              key={subregionOption}
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mr-4 mb-4 ${
+                subregion === subregionOption ? "bg-blue-700" : ""
+              }`}
+              onClick={() => setsubregion(subregionOption)}
+            >
+              {subregionOption}
             </button>
           ))}
         </div>
@@ -59,7 +101,8 @@ const ImageGallery: React.FC = () => {
       </div>
       <div className="relative bg-slate-700">
         <div className="flex content-center bg-slate-800 w-full ">
-          <ImagesList country={country} />
+          <h3>{subregion}</h3>
+          <ImagesList subregion={subregion}/>
         </div>
       </div>
       
